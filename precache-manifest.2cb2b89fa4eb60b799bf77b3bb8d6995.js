41b5a69718fbf4ecc576ee5f7dae70eea5f6b20f
八宝粥 <1099004091@qq.com>
2021-08-02 13:55:41 +0800

    deploy

---
+++ precache-manifest.2cb2b89fa4eb60b799bf77b3bb8d6995.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4015cc45c6df3e32ca22",
+    "url": "/morney-website/css/app.1c45c337.css"
+  },
+  {
+    "revision": "3dbede6f9ec44a3b2a0a",
+    "url": "/morney-website/css/chunk-vendors.978ec01f.css"
+  },
+  {
+    "revision": "25e2a21bbbe23529f4a6dffdf9175d78",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "4015cc45c6df3e32ca22",
+    "url": "/morney-website/js/app.c1fbb881.js"
+  },
+  {
+    "revision": "3dbede6f9ec44a3b2a0a",
+    "url": "/morney-website/js/chunk-vendors.8717e9e7.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "74c69950a028c39e4d97794d0d682f31",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
